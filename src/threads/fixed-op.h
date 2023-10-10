2887c133af2b7e5034060c736ee9bb877e01f735
minsusun <poodding397@gmail.com>
2023-10-10 23:27:55 +0900

    Fix: fixed-op.h

--- src/threads/fixed-op.h
+++ src/threads/fixed-op.h
@@ -10,7 +10,7 @@ int fp_div (int x, int y);
 
 int int_fp (int n) { return n * _F; }
 int fp_int (int x) { return x / _F; }
-int fp_int_round (int x) { return (x >= 0) ? ((x + _F / 2) / _F) : ((x +- _F / 2) / _F); }
+int fp_int_round (int x) { return (x >= 0) ? ((x + _F / 2) / _F) : ((x + _F / 2) / _F); }
 int fp_add (int x, int y) { return x + y; }
 int fp_mul (int x, int y) { return ((int64_t) x) * y / _F; }
 int fp_div (int x, int y) { return ((int64_t) x) * _F / y; }
