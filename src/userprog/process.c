f24bf883dc20e8bdb34bee8e53fb16f0a0a66c82
minsusun <poodding397@gmail.com>
2023-12-03 02:26:57 +0900

    feat: use frame table in setup_stack

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -24,6 +24,9 @@
 /* Lab2 - fileSystem */
 #include "userprog/syscall.h"
 
+/* lab3 - frame table */
+#include "vm/falloc.h"
+
 /* lab3 - supplemental page table */
 #include "vm/spt.h"
 
@@ -564,14 +567,18 @@ setup_stack (void **esp)
   uint8_t *kpage;
   bool success = false;
 
-  kpage = palloc_get_page (PAL_USER | PAL_ZERO);
+  /* lab3 - frame table */
+  // kpage = palloc_get_page (PAL_USER | PAL_ZERO);
+  kpage = falloc_get_page (PAL_USER | PAL_ZERO, PHYSE_BASE - PGSIZE);
   if (kpage != NULL) 
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
         *esp = PHYS_BASE;
       else
-        palloc_free_page (kpage);
+        /* lab3 - frame table */
+        // palloc_free_page (kpage);
+        falloc_free_page (kpage);
     }
   return success;
 }
